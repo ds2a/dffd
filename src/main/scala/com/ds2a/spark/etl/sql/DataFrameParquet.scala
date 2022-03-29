06bcfe5385f6ade0cc6df092c8fe187574a8afb5
akivi <akiviswanath@gmail.com>
2022-03-29 23:30:25 +0530

    simple Create RDD example

---
+++ src/main/scala/com/ds2a/spark/etl/sql/DataFrameParquet.scala
@@ -0,0 +1,21 @@
+package com.ds2a.spark.etl.sql
+
+import org.apache.spark.sql.SparkSession
+
+
+object DataFrameParquet {
+  def main(args: Array[String]): Unit = {
+    val spark : SparkSession = SparkSession.builder().master("local[1]")
+      .appName("sparkByExamples.com")
+      .getOrCreate()
+    val df =  spark.read.parquet("C:\\Users\\akivi\\IdeaProjects\\spark-etl\\input\\userdata1.parquet")
+    df.printSchema()
+    df.show {
+      false
+    }
+    df.createOrReplaceTempView("ParquetTable")
+    val parkSQl = spark.sql("select * from ParquetTable where (gender = 'Male')and(country ='Russia')")
+    parkSQl.show()
+    parkSQl.printSchema()
+  }
+}
