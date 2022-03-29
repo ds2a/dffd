d4fa5cc743371d69c62d0fbdcd977b1caaa97757
akivi <akiviswanath@gmail.com>
2022-03-29 18:27:39 +0530

    simple Create RDD example

---
+++ src/main/scala/com/ds2a/spark/etl/sql/CreateDf.scala
@@ -0,0 +1,24 @@
+package com.ds2a.spark.etl.sql
+
+import org.apache.spark.sql.SparkSession
+
+
+object CreateDf {
+  def main(args: Array[String]): Unit = {
+    val spark : SparkSession = SparkSession.builder().master("local[1]")
+      .appName("sparkByExamples.com")
+      .getOrCreate()
+    val columns = Seq("language","users_count")
+    val data = Seq(("Java","20000"),("Python","10000"),("scala","3000"))
+    val rdd = spark.sparkContext.parallelize(data)
+    import spark.implicits._
+    val dfFromRdd = rdd.toDF()
+    dfFromRdd.printSchema()
+
+    val dfFromRdd2 = rdd.toDF("language","users_count")
+    dfFromRdd2.printSchema()
+
+    val dfFromRdd3 = spark.createDataFrame(rdd).toDF(columns:_*)
+    dfFromRdd3.printSchema()
+  }
+}
