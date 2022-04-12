559824c993a0b57033439467323b9483caa87c4f
akivi <akiviswanath@gmail.com>
2022-04-12 10:52:45 +0530

    simple Create RDD example

--- src/main/scala/com/ds2a/spark/etl/sql/createAnilDf.scala
+++ src/main/scala/com/ds2a/spark/etl/sql/createAnilDf.scala
@@ -1,7 +1,8 @@
 package com.ds2a.spark.etl.sql
 
 import org.apache.spark.sql.Row.empty.schema
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.functions.to_timestamp
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType, TimestampType}
 import org.apache.spark.sql.{Row, SparkSession}
 
 object createAnilDf {
@@ -9,17 +10,26 @@ object createAnilDf {
     val spark : SparkSession = SparkSession.builder().master("local[1]")
       .appName("sparkByExamples.com")
       .getOrCreate()
+    spark.sparkContext.setLogLevel("error")
     val data = Seq(Row("James","","Smith","1991-04-01","M",3000),
-    Row("Michael","Rose","","2000-05-19","M",4000),
-    Row("Robert","","Williams","1978-09-05","M",4000),
-    Row("Maria","Anne","Jones","1967-12-01","F",4000),
-    Row("Jen","Mary","Brown","1980-02-17","F",-1))
+    Row("Michael","Rose","","20000519","M",4000),
+    Row("Robert","","Williams","19780905","M",4000),
+    Row("Maria","Anne","Jones","19671201","F",4000),
+    Row("Jen","Mary","Brown","19800217","F",-1))
+    val data1 = Seq(("James","","Smith","1991-04-01","M",3000),
+      ("Michael","Rose","","20000519","M",4000),
+      ("Robert","","Williams","19780905","M",4000),
+      ("Maria","Anne","Jones","19671201","F",4000),
+      ("Jen","Mary","Brown","19800217","F",-1))
     val schema = StructType(Array(StructField("firstname",StringType,nullable = true),StructField("middlename",StringType,nullable = true),StructField("lastname",StringType,nullable = true),StructField("dob",StringType,nullable = true),StructField("gender",StringType,nullable = true),StructField("salary",IntegerType,nullable = true)))
     val columns = Seq("firstname","middlename","lastname","dob","gender","salary")
     import scala.collection.JavaConversions._
+    import spark.implicits._
     val df = spark.createDataFrame(data,schema)
-    df.printSchema()
+    val df1 = spark.createDataFrame(data1).toDF(columns:_*)
     df.show()
-
+    df.printSchema()
+    df1.printSchema()
+    df1.show()
   }
 }
