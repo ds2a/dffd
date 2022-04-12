060a30db739a421b9c9900aa504be3d8153e3282
akivi <akiviswanath@gmail.com>
2022-04-12 11:47:15 +0530

    simple Create RDD example

--- src/main/scala/com/ds2a/spark/etl/sql/createAnilDf.scala
+++ src/main/scala/com/ds2a/spark/etl/sql/createAnilDf.scala
@@ -1,35 +1,45 @@
 package com.ds2a.spark.etl.sql
 
 import org.apache.spark.sql.Row.empty.schema
-import org.apache.spark.sql.functions.to_timestamp
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType, TimestampType}
+import org.apache.spark.sql.functions.{col, to_date, to_timestamp}
+import org.apache.spark.sql.types.{DateType, IntegerType, StringType, StructField, StructType, TimestampType}
 import org.apache.spark.sql.{Row, SparkSession}
 
 object createAnilDf {
   def main(args: Array[String]): Unit = {
-    val spark : SparkSession = SparkSession.builder().master("local[1]")
+    val spark: SparkSession = SparkSession.builder().master("local[1]")
       .appName("sparkByExamples.com")
       .getOrCreate()
     spark.sparkContext.setLogLevel("error")
-    val data = Seq(Row("James","","Smith","1991-04-01","M",3000),
-    Row("Michael","Rose","","20000519","M",4000),
-    Row("Robert","","Williams","19780905","M",4000),
-    Row("Maria","Anne","Jones","19671201","F",4000),
-    Row("Jen","Mary","Brown","19800217","F",-1))
-    val data1 = Seq(("James","","Smith","1991-04-01","M",3000),
-      ("Michael","Rose","","20000519","M",4000),
-      ("Robert","","Williams","19780905","M",4000),
-      ("Maria","Anne","Jones","19671201","F",4000),
-      ("Jen","Mary","Brown","19800217","F",-1))
-    val schema = StructType(Array(StructField("firstname",StringType,nullable = true),StructField("middlename",StringType,nullable = true),StructField("lastname",StringType,nullable = true),StructField("dob",StringType,nullable = true),StructField("gender",StringType,nullable = true),StructField("salary",IntegerType,nullable = true)))
-    val columns = Seq("firstname","middlename","lastname","dob","gender","salary")
+    val data = Seq(Row("James", "", "Smith", "1991-04-01", "M", 3000),
+      Row("Michael", "Rose", "", "2000-05-19", "M", 4000),
+      Row("Robert", "", "Williams", "1978-09-05", "M", 4000),
+      Row("Maria", "Anne", "Jones", "1967-12-01", "F", 4000),
+      Row("Jen", "Mary", "Brown", "1980-02-17", "F", -1))
+    val data1 = Seq(("James", "", "Smith", "1991-04-01", "M", 3000),
+      ("Michael", "Rose", "", "2000-05-19", "M", 4000),
+      ("Robert", "", "Williams", "1978-09-05", "M", 4000),
+      ("Maria", "Anne", "Jones", "1967-12-01", "F", 4000),
+      ("Jen", "Mary", "Brown", "1980-02-17", "F", -1))
+    val schema = StructType(Array(StructField("firstname", StringType, nullable = true), StructField("middlename",
+      StringType, nullable = true),
+      StructField("lastname", StringType, nullable = true), StructField("dob", StringType, nullable = true),
+      StructField("gender", StringType, nullable = true), StructField("salary", IntegerType, nullable = true)))
+    val columns = Seq("firstname", "middlename", "lastname", "dob", "gender", "salary")
     import scala.collection.JavaConversions._
     import spark.implicits._
-    val df = spark.createDataFrame(data,schema)
-    val df1 = spark.createDataFrame(data1).toDF(columns:_*)
+
+    val df = spark.createDataFrame(data, schema)
+
+    val df1 = spark.createDataFrame(data1).toDF(columns: _*)
     df.show()
     df.printSchema()
-    df1.printSchema()
-    df1.show()
+    val df3 = df1.withColumn("dob",col("dob").cast(TimestampType))
+    df3.show()
+    val df2 = df1.withColumn("dob1", to_timestamp(col("dob"))).withColumn("dob2", to_date(col("dob")))
+    df2.printSchema()
+    df2.show()
+    /*Using Rdd's
+     */
   }
 }
