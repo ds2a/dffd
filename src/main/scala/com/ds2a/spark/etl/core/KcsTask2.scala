fe08122692b3a2aec26db7a6981bb1a41360ef67
akivi <akiviswanath@gmail.com>
2022-06-17 02:41:16 +0530

    commit my messages

--- src/main/scala/com/ds2a/spark/etl/core/KcsTask2.scala
+++ src/main/scala/com/ds2a/spark/etl/core/KcsTask2.scala
@@ -1,8 +1,28 @@
 package com.ds2a.spark.etl.core
+/*
+. Create a DataFrame from the "Courses" datasets. And given three fields as column name below. a. course_id b. course_name c. course_fee
+3. Using the Case Class named Learner and create an RDD for second dataset. a. name b. email c. city
+4. Now show how can you create an RDD into DataFrame.
+5. Now show how can you convert a DataFrame to Dataset.
+--- Courses ----
+1001, "Hadoop" , 7000
+1002, "Spark" , 7000
+1003, "Cassandra" , 7000
+1004, "Python" , 7000
 
+--- Learner Students -------------
+"Amit" , "amit@hadoopexam.com", "Mumbai"
+"Rakesh" , "rakesh@hadoopexam.com", "Pune"
+ "Jonathan" , "jonathan@hadoopexam.com", "NewYork"
+ "Michael" , "michael@hadoopexam.com", "Washington"
+"Simon" , "simon@hadoopexam.com", "HongKong"
+"Venkat" , "venkat@hadoopexam.com", "Chennai"
+ "Roshni" , "roshni@hadoopexam.com", "Bangalore"
+
+*/
 import org.apache.spark.sql.SparkSession
 
-case class Learner(name:String,email:String,city:String);
+case class Learner(name:String,email:String,city:String)
 
 object KcsTask2 {
   def main(args: Array[String]): Unit = {
@@ -10,6 +30,7 @@ object KcsTask2 {
     val courseData = Seq((1001,"Hadoop",7000 ),(1002,"Spark",7000),(1003,"Cassandra",7000),(1004,"Python",7000))
     import spark.implicits._
     val CourseDf = courseData.toDF("course_id","course_name","course_fee")
+    CourseDf.show()
 
     val LearnersData = Array(
       Learner("Amit","amit@hadoopexam.com","Mumbai"),
@@ -18,7 +39,7 @@ object KcsTask2 {
       Learner( "Michael","michael@hadoopexam.com","Washington"),
       Learner("Simon","simon@hadoopexam.com","HongKong" ),
       Learner("Venkat","venkat@hadoopexam.com","Chennai"),
-      Learner( "Roshni","roshni@hadoopexam.com","Banglore")
+      Learner( "Roshni","roshni@hadoopexam.com","Bangalore")
     )
 
     val learnersDataRdd = spark.sparkContext.parallelize(LearnersData)
---
+++ src/main/scala/manohar/friendsByAgeRdd.scala
@@ -0,0 +1,21 @@
+package manohar
+
+import org.apache.spark.sql.SparkSession
+
+object friendsByAgeRdd {
+  def main(args: Array[String]): Unit = {
+    val spark:SparkSession = SparkSession.builder().master("local[1]").appName("friendsByAge").getOrCreate()
+    def parseLine(line:String) = {
+      val fields = line.split(",")
+      val age = fields(2).toInt
+      val noOfFriends = fields(3).toInt
+      (age,noOfFriends)
+    }
+    val lines = spark.sparkContext.textFile("C:\\SparkScalaCourse\\data\\fakefriends-noheader.csv")
+    val rdd = lines.map(parseLine)
+    val totalsByAge = rdd.mapValues(x=>(x,1)).reduceByKey((x,y)=> (x._1+y._1,x._2+y._2))
+    val averagesByAge = totalsByAge.mapValues(x=>x._1/x._2)
+    val sortedAveragesByAge = averagesByAge.sortByKey(ascending = true)
+    sortedAveragesByAge.foreach(println)
+  }
+}
